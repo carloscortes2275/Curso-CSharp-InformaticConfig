b870c1ef8bc9167367fdcacef24b904bac914ccd
Carlos Cortés <49925951+carloscortes2275@users.noreply.github.com>
2026-01-20 22:17:12 -0600

    Add files via upload

---
+++ 2. TIPOS DE DATOS EN C#/TiposDeDatos/TiposDeDatos/Program.cs
@@ -0,0 +1,14 @@
+﻿int entero = 1; // 4 Bytes - Números enteros (sin decimales).
+double doble = 3.14; // 8 Bytes - Decimales de alta precisión (estándar).
+bool bandera = true; // 1 Byte - Verdadero o Falso
+char caracter = 'A'; // 2 Bytes - Una sola letra o símbolo.
+float flotante = 1.50f; // 4 Bytes - Decimales sencillos (ej. coordenadas).
+string cadena = "Esto es una cadena"; // Variable, 2 Bytes por caracter - Texto, palabras, párrafos.
+
+Console.WriteLine(entero);
+Console.WriteLine(doble);
+Console.WriteLine(bandera);
+Console.WriteLine(caracter);
+Console.WriteLine(flotante);
+Console.WriteLine(cadena);
+//object, class, interface, etc se tomaran más adelante en el curso
