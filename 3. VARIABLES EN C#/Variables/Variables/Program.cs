001b51b37d9a6e2938ddb0b04de166155cfbf527
Carlos Cortés <49925951+carloscortes2275@users.noreply.github.com>
2026-01-21 22:04:58 -0600

    Add files via upload

---
+++ 3. VARIABLES EN C#/Variables/Variables/Program.cs
@@ -0,0 +1,17 @@
+﻿string miNombre = "Carlos Cortés";
+int edad_1 = 28;
+bool ACTIVO = true;
+DateTime _fecha = DateTime.Now;
+float precio = 14.55f;
+decimal descuento = 10.50m;
+double descuento_2 = 10.50;
+
+Console.WriteLine(miNombre);
+Console.WriteLine(edad_1);
+Console.WriteLine(ACTIVO);
+Console.WriteLine(_fecha);
+Console.WriteLine(precio);
+Console.WriteLine(descuento);
+Console.WriteLine(descuento_2);
+
+Console.ReadKey();
