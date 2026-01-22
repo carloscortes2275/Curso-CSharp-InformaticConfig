250412853f16fae73668bf9a19c2d13ecff70429
Carlos Cortés <49925951+carloscortes2275@users.noreply.github.com>
2026-01-21 22:24:36 -0600

    Add files via upload

---
+++ 4. CONCATENACION DE VALORES/ConcatenacionDeValores/ConcatenacionDeValores/Program.cs
@@ -0,0 +1,13 @@
+﻿string nombre = "Ana";
+int edad = 30;
+
+Console.WriteLine("Nombre de usuario: " + nombre + "\n" + "Edad: " + edad);
+
+Console.WriteLine("Nombre de usuario: " + nombre);
+Console.WriteLine("Edad: " + edad);
+
+Console.WriteLine($"Nombre de usuario: {nombre}, edad: {edad}");
+
+Console.WriteLine($"Nombre de usuario: {nombre} \nedad: {edad}");
+
+Console.ReadKey();
