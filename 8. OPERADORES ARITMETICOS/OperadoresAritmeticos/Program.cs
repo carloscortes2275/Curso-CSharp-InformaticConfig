896d4d12d211046d5c09d02e3c6643f7a264a806
Carlos Cortés <49925951+carloscortes2275@users.noreply.github.com>
2026-01-22 23:10:16 -0600

    Add files via upload

---
+++ 8. OPERADORES ARITMETICOS/OperadoresAritmeticos/Program.cs
@@ -0,0 +1,27 @@
+﻿int valor1 = 90, valor2 = 70;
+var valor3 = 2.3;
+
+// Operador de suma o adición
+int total = valor1 + valor2;
+Console.WriteLine($"Total de la suma: {total}");
+
+// Resta
+int diferencia = valor1 - valor2;
+Console.WriteLine($"Diferencia: {diferencia}");
+
+// Multiplicación
+var producto = valor3 * valor2;
+Console.WriteLine($"Multiplicación de {valor3} * {valor2}: {producto}");
+
+// División
+var resultado = valor1 / valor3;
+Console.WriteLine($"División entre {valor1}/{valor3}: {resultado}");
+
+// Residuo
+var modulo = valor2 % valor3;
+Console.WriteLine($"Residuo de dividir {valor2}/{valor3}: {modulo}");
+
+
+
+
+Console.ReadKey();
