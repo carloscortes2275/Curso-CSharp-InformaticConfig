71284bf867744374263a2e12b2c31e9ffa24355f
Carlos Cortés <49925951+carloscortes2275@users.noreply.github.com>
2026-01-22 22:53:23 -0600

    Add files via upload

---
+++ 7. CASTING/Casting/Program.cs
@@ -0,0 +1,26 @@
+﻿// Casting implicito
+int valor = 200;
+double total = valor;
+
+Console.WriteLine($"Implicito: {total}");
+
+// Casting explicito
+double precio = 500.23;
+int descuento = (int)precio;
+
+Console.WriteLine($"Explicito: {descuento}");
+
+char letra = 'A';
+int codigoAscii = letra;
+Console.WriteLine($"Código Ascii: {codigoAscii}");
+
+string palabra = "123456";
+int numero = Convert.ToInt32(palabra); // Funcion para convertir strings a enteros
+Console.WriteLine($"String a int: {numero}");
+
+string textoDecimal = "150.60";
+double valor3 = Double.Parse(textoDecimal); // Valor texto a double
+Console.WriteLine($"String a double: {valor3}");
+
+
+Console.ReadKey();
