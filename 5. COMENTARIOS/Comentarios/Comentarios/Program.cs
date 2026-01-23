18d7c89612552fd8ca09ae2b4b1dfe6310596be2
Carlos Cortés <49925951+carloscortes2275@users.noreply.github.com>
2026-01-22 22:17:44 -0600

    Add files via upload

---
+++ 5. COMENTARIOS/Comentarios/Comentarios/Program.cs
@@ -0,0 +1,15 @@
+﻿// Uso de la concatenación en C#
+// Cambios hechos en 22 de Enero 2026
+string nombre = "Ana";
+int edad = 30;
+
+// Salida de datos de las variables
+
+/*Console.WriteLine("Nombre de usuario: " + nombre + "\n" + "Edad: " + edad);
+Console.WriteLine("Nombre de usuario: " + nombre);
+Console.WriteLine("Edad: " + edad);
+Console.WriteLine($"Nombre de usuario: {nombre}, edad: {edad}"); */
+
+Console.WriteLine($"Nombre de usuario: {nombre} \nedad: {edad}");
+
+//Console.ReadKey();
