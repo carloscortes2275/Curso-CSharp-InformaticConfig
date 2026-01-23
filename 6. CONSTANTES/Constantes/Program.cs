4b968fd8be349f2332cbea5583ba4159344c58fa
Carlos Cortés <49925951+carloscortes2275@users.noreply.github.com>
2026-01-22 22:36:04 -0600

    Add files via upload

---
+++ 6. CONSTANTES/Constantes/Program.cs
@@ -0,0 +1,21 @@
+﻿var numero = 10.20; // Permite tipos de datos variables
+
+Console.WriteLine(numero);
+
+numero = 3000;
+
+Console.WriteLine(numero);
+
+int numero2 = 150;
+Console.WriteLine($"Valor de constante: {numero2}");
+numero2 = 80;
+Console.WriteLine(numero2);
+
+
+const double PI = 3.1416;
+Console.WriteLine(PI);
+
+const string mensaje = "BIENVENIDOS";
+Console.WriteLine(mensaje);
+
+Console.ReadKey();
